a8e0c76d8fc30195477c1be53f4f44a6e0b3dcad
Icaro Matos <icaroamb@gmail.com>
2025-06-02 23:42:22 -0300

    Delete controller directory

--- controller/extension/payment/d5gn_lightbox.php
+++
@@ -1,3 +0,0 @@
-<?php D5W::_(); return;
-// © Dev5™. Todos os direitos reservados ~ dev5.com.br
-// Distribuição/reprodução não autorizada estará sujeita às penas das leis nº 9.609/98 e nº 9.610/98
--- controller/extension/paypal_plus/menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Text
-$_['text_paypal_plus']             = 'PayPal Plus';
-$_['text_paypal_plus_transaction'] = 'Transações';
-$_['text_paypal_plus_log']         = 'Logs da API';
